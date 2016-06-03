ecb828421bafa4e05dfc89a5e39b97813a459fea
Dean Chuang <azurenokite@gmail.com>
2016-06-03 02:23:21 +0000

    add node-sass and node-sass-middleware

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var path = require('path');
+var sass = require('node-sass-middleware');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 var filePath = path.resolve(__dirname, 'public');
+var sassPath = path.resolve(__dirname, 'sass');
+
+// set up sass middleware to convert scss into css
+app.use(
+  sass({
+    src: sassPath,
+    dest: filePath + '/css',
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/css' // crucial line to get the css rendered to the right place
+  })
+);
 
 // set up public directory to serve files
 app.use(express.static(filePath));
