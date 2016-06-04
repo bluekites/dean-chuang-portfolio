73597ed5658a0dca1c2d0fefaed929c3d9754355
Dean Chuang <azurenokite@gmail.com>
2016-06-04 04:08:43 +0000

    add nodemailer and nodemailer-smtp-transport features

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-  $('h1').on('click', function(){
-    $('h1').css('color', 'red');
+  
+  
+  var email;
+  var name;
+  var content;
+  var message = $('#message');
+  $('#submit').click(function(){
+    email = $('#email').val();
+    name = $('#name').val();
+    content = $('#content').val();
+    message.text('Sending your message...Please wait');
+    $.get('/send', {email: email, name: name, content: content}, function(data){
+      if(data === 'sent') {
+        message.text('Message sent! Thank you for contacting us!');
+      }
+    });
   });
 });
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var path = require('path');
 var sass = require('node-sass-middleware');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 var app = express();
 
 var EMAIL = require('../hidden_data/email'); // environment variable for mailing information
 var PORT = process.env.PORT || 3000;
 var filePath = path.resolve(__dirname, 'public');
 var sassPath = path.resolve(__dirname, 'sass');
-
 
 // set up sass middleware to convert scss into css
 app.use(
@@ -20,12 +21,41 @@ app.use(
   })
 );
 
+// mailer smtpTransporter configuration
+var transporter = nodemailer.createTransport(smtpTransport({
+  service: 'Gmail',
+  secureConnection: false,
+  port: 587,
+  auth: {
+    user: EMAIL.username,
+    pass: EMAIL.password
+  }
+}));
+
 // set up public directory to serve files
 app.use(express.static(filePath));
+
 
 // root path serves up index.html
 app.get('/', function(req, res){
   res.sendFile('index.html');
+});
+
+app.get('/send', function(req, res){
+  var mailerOptions = {
+    to: 'azurenokite@gmail.com',
+    from: req.query.email,
+    subject: req.query.name,
+    text: req.query.content
+  };
+  console.log(mailerOptions);
+  transporter.sendMail(mailerOptions, function(err, info){
+    if(err){
+      return console.log(err);
+    } 
+    console.log('Message sent successfully! ' + info.response);
+    res.end('sent');
+  });
 });
 
 app.listen(PORT, function(){
