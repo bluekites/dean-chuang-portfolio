a3b81bf5f3b92a9c57b8f118aec34c282415e025
Dean Chuang <azurenokite@gmail.com>
2016-07-08 17:42:41 +0000

    refactor jQuery selectors to improve performance

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   
+  // declaring jquery selectors
+  var $window = $(window);
+  var $logo = $('#logo');
+  var $row = $('.row');
+  var $contact = $('#contact-section');
+  var $footer = $('#footer');
+  var $overlay = $('.overlay-container');
+  
   // loading screen effect
   $(window).load(function(){
     $('#loader-wrapper').fadeOut('slow');
@@ -7,41 +15,41 @@ $(document).ready(function(){
   
   // scrolling effects
   $(window).scroll(function(){
-    // declaring windowScroll
+    // declaring windowscroll
     var windowScroll = $(this).scrollTop();
     
     // masthead parallax effect
-    $('#logo').css({
+    $logo.css({
       'transform': 'translate(0px, ' + windowScroll/2 + '%)'
     });
     
     // project section parallax effect
-    $('.row').each(function(i){
+    $row.each(function(i){
       // won't trigger until the rows are within 50% of the browser
-      if(windowScroll > $('.row').eq(i).offset().top - ($(window).height() / 1.3)) {
+      if(windowScroll > $row.eq(i).offset().top - ($window.height() / 1.3)) {
         if(i % 2 === 0) {
-          $('.row').eq(i).addClass('evenshow');
+          $row.eq(i).addClass('evenshow');
         } else {
-          $('.row').eq(i).addClass('oddshow');
+          $row.eq(i).addClass('oddshow');
         }
       }
     });
     
     // form fade effect
-    if(windowScroll > $('#contact-section').offset().top - ($(window).height() / 1.4)) {
+    if(windowScroll > $contact.offset().top - ($window.height() / 1.4)) {
       $('form').css('opacity', '1');
     }
     
     // footer fade effect
     if(windowScroll !== 0) {
-      $('#footer').fadeIn('slow');
+      $footer.fadeIn('slow');
     } else {
-      $('#footer').fadeOut('slow');
+      $footer.fadeOut('slow');
     }
   });
   
   // footer hover animation 
-  $('#footer').on('mouseenter', function(){
+  $footer.on('mouseenter', function(){
     $(this).animate({'opacity': '1'}, 500);
   }).on('mouseleave', function(){
     $(this).animate({'opacity': '0.5'}, 500);
@@ -54,7 +62,7 @@ $(document).ready(function(){
   });
   
   // project section overlay
-  $('.overlay-container').on('mouseenter', function(){
+  $overlay.on('mouseenter', function(){
     $(this).children('img').addClass('zoom-in');
     $(this).children('.overlay').fadeToggle('slow');
   }).on('mouseleave', function(){
